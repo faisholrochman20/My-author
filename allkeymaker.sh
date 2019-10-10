7eb28f9d171a2028a171d778f1e20b493d87ef21
faisholrochman20 <54407823+faisholrochman20@users.noreply.github.com>
2019-10-10 18:17:38 +0700

    Add files via upload

---
+++ allkeymaker.sh
@@ -0,0 +1,28 @@
+#!bin/sh
+
+w='\e[97m'         #putih
+g='\033[1;92m'     #hijau
+r='\033[1;91m'     #merah
+b='\033[1;94m'     #biru
+uline='\e[1;4m'    #bergarisbawah
+c='\033[1;36m'     #cyan
+red='\033[1;31m'
+k='\033[1;33m'     #kuning
+u='\033[1;35m'     #ungu
+re='\033[0m'       #putih
+
+echo "$c    ▄▄     ▄▄▄▄      ▄▄▄▄      ▄▄"
+echo "   ████    ▀▀██      ▀▀██      ██"
+echo "   ████      ██        ██      ██ ▄██▀    ▄████▄   ▀██  ███"
+echo "  ██  ██     ██        ██      ██▄██     ██▄▄▄▄██   ██▄ ██"
+echo "  ██████     ██        ██      ██▀██▄    ██▀▀▀▀▀▀    ████▀"
+echo " ▄██  ██▄    ██▄▄▄     ██▄▄▄   ██  ▀█▄   ▀██▄▄▄▄█     ███"
+echo " ▀▀    ▀▀     ▀▀▀▀      ▀▀▀▀   ▀▀   ▀▀▀    ▀▀▀▀▀      ██"
+echo "                                                    ███$k\n"
+sleep 2
+echo "========================<Welcome To Rohman Project>========================\n"
+echo $c'Author By       '$re':'$g' Faishol Rochman'$re'        ©R_P'
+echo $c'Telegram        '$re':'$g' 083856539185'$re'          v 1.0'
+echo $c'Whatsapp        '$re':'$g' 085708903896'
+echo $c'Channel Youtube '$re':'$g' Rohman Music'
+echo $c'IG              '$re':'$g' faishol_rochman20'
